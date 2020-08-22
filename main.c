401f9a64cc3abacc9f89876aed2757818436a967
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-22 18:22:26 -0400

    Help command now works.

--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void CL() {
-    char *command;
 
-    printf("Enter a command: ");
-    scanf(" %c", &command);
-
-    if ((command = "help")) {
+    char command[50];
+    printf("Enter command: ");
+    scanf("%s", command);
+    if(!strcmp(command, "help")) {
         system("cat helpFiles/FOMOSCL.txt");
-    } else {
-        printf("Uknown");
     }
-    exit(0);
+    else {
+        printf("Unknown command \n");
+    }
+
 }
 
 int main() {
     printf("FOMOSv2-CL \n");
 
-    CL();
+    for (;;) {
+        CL();
+    }
 
     return 0;
 }
