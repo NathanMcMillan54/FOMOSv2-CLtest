586153d8b6645ff733188c4bfded225253d2c54d
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-23 07:02:09 -0400

    Restart now works.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int powerOff = 1;
+
 void CL() {
 
     char command[50];
@@ -9,6 +11,15 @@ void CL() {
     scanf("%s", command);
     if(!strcmp(command, "help")) {
         system("cat helpFiles/FOMOSCL.txt");
+    } else if (!strcmp(command, "powerOff")) {
+        system("gcc -Wall power/powerOff.c -o power/powerOff");
+        system("./power/powerOff");
+        powerOff = 0;
+    } else if (!strcmp(command, "restart")) {
+        system("gcc -Wall power/restart.c -o power/restart");
+        printf("Enter your password \n");
+        system("sudo ./power/restart");
+        powerOff = 0;
     }
     else {
         printf("Unknown command \n");
@@ -21,7 +32,9 @@ int main() {
 
     for (;;) {
         CL();
+        if (powerOff == 0) {
+            printf("Goodbye \n");
+            exit(0);
+        }
     }
-
-    return 0;
 }
--- power/powerOff.c
+++ power/powerOff.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("power off");
+    printf("power off \n");
     return 0;
 }
