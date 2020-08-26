e507df7c398901eb32f71e59bcff8c9886f480ee
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-26 15:32:47 -0400

    Added iwi option to help.c for anyone using the help command.

--- helpFiles/help.c
+++ helpFiles/help.c
@@ -8,6 +8,7 @@ int main() {
     printf("1) FOMOSCL \n");
     printf("2) Commands \n");
     printf("3) Problems \n");
+    printf("4) iwi \n");
 
     printf("Enter a number: ");
     scanf("%d", &option);
@@ -22,6 +23,8 @@ int main() {
         case 3 :
             system("cat helpFiles/problems.txt");
             break;
+        case 4:
+            system("cat helpFiles/iwi.txt");
         default :
             printf("NaN \n" );
             printf("Exiting help menu... \n");
