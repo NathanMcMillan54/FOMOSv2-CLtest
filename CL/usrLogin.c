7119608f21eac34c53f1758edf7cfb73869d3cfb
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-26 11:35:37 -0400

    Started working on a command that will install things.

--- CL/usrLogin.c
+++ CL/usrLogin.c
@@ -8,4 +8,5 @@ void login() {
 
 int main() {
     printf("Login to");
+    login();
 }
---
+++ install/install.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("This file is for installing stuff from git.");
+    return 0;
+}
